f56ca40eb083d04046adf33821a45a1bf0afc652
Saranya Nayak <san00kam1@proton.me>
2026-05-28 01:42:09 +0530

    Refactor timeline scripts and styles; modularize getDays function and adjust timeline width

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -1,21 +1,26 @@
+import { getDays } from "./utils.js";
+
 var timeline = document.getElementById("timeline");
 
-var getDays = (start, end) => {
-    const startDate = new Date(start);
-    const endDate = new Date(end);
-    const timeDiff = endDate.getTime() - startDate.getTime();
-    const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
-    return daysDiff;
-}
-
-const days = getDays("2025-09-21", "2025-11-30");
+const startDateStr = "2025-09-21"; 
+const endDateStr = "2025-11-30";
+const days = getDays(startDateStr, endDateStr);
+const startDate = new Date(startDateStr); 
 
 for (let i = 0; i < days; i++) {
     const dayElement = document.createElement("div");
+    
+    //  assigne date as a data attribute to each day element
+    const current = new Date(startDate);
+    current.setDate(startDate.getDate() + i); 
+    const dateStr = current.toISOString().split("T")[0];
+
+    // set class and id for each day element
+    dayElement.dataset.date = dateStr;
     dayElement.classList.add("day");
-    if (i === 0){dayElement.classList.add("course_start")}
-    if ((i+1)%7 === 0) {dayElement.classList.add("weekend");}
-    if (i === days-1){dayElement.classList.add("course_end")}
+    if (i === 0) { dayElement.classList.add("course_start") }
+    if ((i+1) % 7 === 0) { dayElement.classList.add("weekend") }
+    if (i === days-1) { dayElement.classList.add("course_end") }
     dayElement.id = `Day-${i + 1}`;
     timeline.appendChild(dayElement);
 }
