89675663de8146ba33699834a23860867e5cd7ec
Saranya Nayak <san00kam1@proton.me>
2026-05-28 19:27:49 +0530

    Refactor timeline logic and styles; integrate COURSE_CONFIG for better configuration and enhance milestone handling

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -1,26 +1,35 @@
 import { getDays } from "./utils.js";
 
-var timeline = document.getElementById("timeline");
+const COURSE_CONFIG = {
+    start: "2025-09-21",
+    end: "2025-12-03",
+    milestones: ["2025-10-14", "2025-11-05", "2025-12-03"]
+};
 
-const startDateStr = "2025-09-21"; 
-const endDateStr = "2025-11-30";
-const days = getDays(startDateStr, endDateStr);
-const startDate = new Date(startDateStr); 
+const timeline = document.getElementById("timeline");
+const startDate = new Date(COURSE_CONFIG.start);
+const days = getDays(COURSE_CONFIG.start, COURSE_CONFIG.end);
+let milestoneCounter = 1;
 
-for (let i = 0; i < days; i++) {
+for (let i = 0; i <= days; i++) {
     const dayElement = document.createElement("div");
-    
-    //  assigne date as a data attribute to each day element
     const current = new Date(startDate);
-    current.setDate(startDate.getDate() + i); 
+    current.setDate(startDate.getDate() + i);
     const dateStr = current.toISOString().split("T")[0];
 
-    // set class and id for each day element
     dayElement.dataset.date = dateStr;
-    dayElement.classList.add("day");
-    if (i === 0) { dayElement.classList.add("course_start") }
-    if ((i+1) % 7 === 0) { dayElement.classList.add("weekend") }
-    if (i === days-1) { dayElement.classList.add("course_end") }
     dayElement.id = `Day-${i + 1}`;
+
+    if (COURSE_CONFIG.milestones.includes(dateStr)) {
+        dayElement.classList.add(`milestone-${milestoneCounter}`);
+        milestoneCounter++;
+    }
+    if (i === 0) { dayElement.classList.add("course_start"); }
+    if ((i + 1) % 7 === 0) { dayElement.classList.add("weekend"); }
+    if (i === days) { dayElement.classList.add("course_end"); }
+    dayElement.classList.add("day");
+
     timeline.appendChild(dayElement);
 }
+
+
